cc04f98846584c7636d8559d81b0676a7a040b07
Varun <2003varunp@gmail.com>
2024-10-07 17:11:14 +0530

    some minor constant changes

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,5 +2,5 @@ export const navLinks = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
   { name: "Events", href: "/events" },
-  { name: "Tickets", href: "/tickets" },
+  { name: "Tickets", href: "/ticket" },
 ];
