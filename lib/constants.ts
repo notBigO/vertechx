e69b7a8e8d5951176e6d0431d84340a557079623
Varun <2003varunp@gmail.com>
2024-11-01 12:00:57 +0530

    all registration page setup

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@ export const navLinks = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
   { name: "Events", href: "/events" },
-  { name: "Tickets", href: "/ticket" },
+  { name: "Registrations", href: "/registrations" },
 ];
 
 export const categories = [
