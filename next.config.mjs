66ea1a0e0dfdad7a85a9153bef44b824c2e0a917
Varun <2003varunp@gmail.com>
2024-11-26 23:04:27 +0530

    added cloudflare r2 image domain

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,12 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ["images.unsplash.com", "plus.unsplash.com", "res.cloudinary.com"],
+    domains: [
+      "images.unsplash.com",
+      "plus.unsplash.com",
+      "res.cloudinary.com",
+      "pub-ad7b9dfb5d1942639c6f3b5196e947c8.r2.dev",
+    ],
   },
 };
 
